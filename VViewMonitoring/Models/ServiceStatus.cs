939ab86a3838a1c35856b670391a8614d9113541
Dheeraj Chopra <dheeraj.chopra@vviewtech.com>
2026-01-09 15:13:41 +0530

    Added Models folder and model classes

---
+++ VViewMonitoring/Models/ServiceStatus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace VViewMonitoring.Models
+{
+    public class ServiceStatus
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ VViewMonitoring/Models/WindowsServiceReader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceProcess;
+using VViewMonitoring.Models;
+
+namespace VViewMonitoring.SystemReaders
+{
+    public static class WindowsServiceReader
+    {
+        public static List<ServiceStatus> GetServices()
+        {
+            var servicesToCheck = new[]
+            {
+                "vwcardservice",
+                "vwbgservice",
+                "vwcfileservice"
+            };
+
+            var result = new List<ServiceStatus>();
+
+            foreach (var serviceName in servicesToCheck)
+            {
+                try
+                {
+                    ServiceController sc = new ServiceController(serviceName);
+
+                    result.Add(new ServiceStatus
+                    {
+                        Name = serviceName,
+                        DisplayName = sc.DisplayName,
+                        Status = sc.Status.ToString()
+                    });
+                }
+                catch (Exception)
+                {
+                    result.Add(new ServiceStatus
+                    {
+                        Name = serviceName,
+                        DisplayName = "Not Installed",
+                        Status = "Not Found"
+                    });
+                }
+            }
+
+            return result;
+        }
+    }
+}
